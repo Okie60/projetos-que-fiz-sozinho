a7a08c6998c9e18010ab72e194f7f8dcaf9b945b
OkieDokie <87245056+Okie60@users.noreply.github.com>
2023-06-17 12:52:41 -0300

    Add files via upload

---
+++ Calculadora simples/Calculadora simples/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+double x, y, result;
+WriteLine("Calculadora simples:\ndigite: x operador y\n");
+x = Convert.ToDouble(ReadLine());
+string op;
+op = ReadLine();
+y = Convert.ToDouble(ReadLine());
+switch (op)
+{
+    case "+":
+        WriteLine($"Sua operação foi: {x} + {y}");
+        result = x + y;
+        WriteLine($"Resultado: {result}");
+        break;
+    case "-":
+        WriteLine($"Sua operação foi: {x} - {y}");
+        result = x - y;
+        WriteLine($"Resultado: {result}");
+        break;
+    case "*":
+        WriteLine($"Sua operação foi: {x} * {y}");
+        result = x * y;
+        WriteLine($"Resultado: {result}");
+        break;
+    case "/":
+        WriteLine($"Sua operação foi: {x} / {y}");
+        result = x / y;
+        WriteLine($"Resultado: {result}");
+        break;
+    default:
+        WriteLine("Insira um operador válido.");
+        break;
+}
